0089eb60636b04c9ac03d434f4f9018bc4798f52
Salem Amba <salemamba@gmail.com>
2016-10-23 11:53:18 -0700

    updates custom.css, omniauth_ctr, mailer views.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,8 +2,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def all
 		@user = User.from_omniauth(request.env["omniauth.auth"])
             # raise request.env["omniauth.auth"].to_yaml 
-    	if @user.persisted?
-    		flash.notice = 'Signed in'
+    	if @user.persisted? 
+    		flash.notice = 'Signed in' if @user.confirmed_at 
      	 	sign_in_and_redirect @user  #, :event => :authentication #this will throw if @user is not activated
      	 	# set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     	else
