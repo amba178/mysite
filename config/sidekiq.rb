1e78fc1977a633e8b7a616b24f24be379b9f87f9
Salem Amba <salemamba@gmail.com>
2016-10-26 13:32:33 -0700

    modifies sidekiq.rb

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,7 +1,16 @@
 require 'sidekiq'
 
+# Sidekiq.configure_client do |config|
+#   config.redis = { size: 1 }
+# end
+
+
+
+redis_conn = proc {
+  Redis.new # do anything you want here
+}
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1 }
+  config.redis = ConnectionPool.new(size: 3, &redis_conn)
 end
 
 
