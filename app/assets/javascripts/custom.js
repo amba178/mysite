bf2d7e0a86501efeb0d721d456cd48430bdc16dd
Salem Amba <salemamba@gmail.com>
2016-12-14 13:29:12 -0800

    implments complete rendering reply and new form for post inline instead of reloading a new page.

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,14 @@
+
+
+$(document).on("turbolinks:load", function() {
+
+	if ($('#new_post').length) {
+
+		$('#new-comment-id').hide();
+		$('li').removeClass("link-list-item")
+       
+	}
+
+	alert("salem");
+});
+
