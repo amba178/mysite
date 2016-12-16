38286c0b5a60938b4b313206b53e4336cf5cbc15
Salem Amba <salemamba@gmail.com>
2016-12-16 10:39:51 -0800

    implements redirecting to previous url after sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, :unless => :devise_controller?
   # before_action :after_sign_in_path_for, :after_sign_out_path_for, if: :devise_controller? 
   # before_action :prepare_meta_tags, if: 'request.get?'
   
@@ -20,9 +21,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def local_request?
-  #   false
-  # end
+  def store_current_location
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+       request.path != "/users/sign_up" &&
+       request.path != "/users/password/new" &&
+       request.path != "/users/password/edit" &&
+       request.path != "/users/confirmation" &&
+       request.path != "/users/sign_out" &&
+       !request.xhr?) # don't store ajax calls
+        session[:previous_url] = request.fullpath 
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :uid, :provider, :image, :auth_token])
@@ -34,13 +44,14 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-  	stored_location_for(resource) ||  request.referer || root_path 
+  	# stored_location_for(resource) ||  request.referer || root_path 
   	
-  	if resource.is_a?(User) 
-      photos_path 
-  	else
-  		super 
-  	end
+  	# if resource.is_a?(User) 
+   #    photos_path 
+  	# else
+  	# 	super 
+  	# end
+    session[:previous_url] || root_path
   end
 
 
