391c5a9aa91e359e773bc406bab33d61de10a7e9
Salem Amba <salemamba@gmail.com>
2016-12-22 10:22:11 -0800

    updates application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Mysite
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+    config.assets.initialize_on_precompile = false
     config.generators do |g|
     	g.test_framework :rspec 
     	# g.factory_girl false 
