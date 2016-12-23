e04122c4906ec6f972f876959a7406f27cc4b95d
Salem Amba <salemamba@gmail.com>
2016-12-22 19:25:31 -0800

    modifies filename

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -54,13 +54,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   def filename
-     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+     "#{secure_token()}.#{file.extension}" if original_filename.present?
   end
 
   protected
   def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+    SecureRandom.hex(length)
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
