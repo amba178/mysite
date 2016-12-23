b26d589600941a9005cb6043032aab7cd394274f
Salem Amba <salemamba@gmail.com>
2016-12-22 18:19:41 -0800

    modifies access photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  # before_action :authenticate, if: -> {Rails.env.production?} 
+  before_action :authenticate, if: -> {Rails.env.production?} 
   before_filter :authenticate_user!, :except => [:show, :index]
   before_action :set_photo, :only => [:show]
 
