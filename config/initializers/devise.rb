f307f65b50786f5efea240c92e697c6ee13e1ae1
Salem Amba <salemamba@gmail.com>
2016-12-23 19:51:15 -0800

    modifies redirect url facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,8 +254,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   # config.omniauth :facebook, "APP_ID", "APP_SECRET", scope: 'email', info_fields: 'email,name'
 
-  config.omniauth :twitter, ENV['CONSUMER_KEY'] , ENV['CONSUMER_SECRET'],    callback_url:    'https://develop-sagan.herokuapp.com/'
-  config.omniauth :facebook, ENV['FACEBOOK_API_ID'], ENV['FACEBOOK_SECRET'], callback_url:    'https://develop-sagan.herokuapp.com/auth/facebook/callback', scope: 'email,publish_actions'
+  config.omniauth :twitter, ENV['CONSUMER_KEY'] , ENV['CONSUMER_SECRET'],     callback_url:    'https://develop-sagan.herokuapp.com/'
+  config.omniauth :facebook, ENV['FACEBOOK_API_ID'], ENV['FACEBOOK_SECRET'],  callback_url:    'https://develop-sagan.herokuapp.com/users/auth/facebook/callback', scope: 'email,publish_actions'
 
 
   # ==> Warden configuration/auth/facebook/callback
