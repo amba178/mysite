9b1ba95ea2c131ce4c23a6ac5449eab06596ec7c
Salem Amba <salemamba@gmail.com>
2017-01-08 17:24:41 -0800

    deletes assets precompile

--- public/assets/custom-4c500dd3a723445d76b84073ae5ecb34edeb9d53b9dd67c7e2cd0877720bd0f9.js
+++
@@ -1,1 +0,0 @@
-ready=function(){},$(document).on("turbolinks:load",function(){$("body").prepend('<div id="fb-root"></div>'),$("#new_post").length&&($("#new-comment-id").hide(),$("li").removeClass("link-list-item"))}),$(document).on("click",".content",function(){});
