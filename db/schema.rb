be24f97c1c2000aa988158b117b46b547765d12a
Salem Amba <salemamba@gmail.com>
2017-03-07 14:03:33 -0800

    runs verson=0 on migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204044938) do
+ActiveRecord::Schema.define(version: 20170213233142) do
 
   create_table "charge_transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "charge_id"
