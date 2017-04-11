0384f70083f9f49b7e0dca9a5eb72366c40b4678
Salem Amba <salemamba@gmail.com>
2017-04-10 20:12:36 -0700

    refactors footer view into helper method to clean it up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,28 @@ module ApplicationHelper
     end.join.html_safe
   end 
 
+  def footer_position(controller_name, action_name, flash=nil)
+     
+  	if['registrations'].include?(controller_name)
+  		if['show', 'edit'].include?(action_name)
+  			return 'footer-absolute'
+  		else
+  			return 'footer-relative'
+  		end
+  	elsif['posts', 'charges'].include?(controller_name)
+  		return 'footer-relative'
+  	elsif['confirmations', 'passwords', 'sessions', 'static_pages'].include?(controller_name)
+  		return 'footer-absolute'
+  	elsif(controller_name=='photos')
+  		if(controller_name.classify.constantize.count < 3 )
+  			return 'footer-absolute'
+  		else
+  			return 'footer-relative'
+  		end
+  	else
+  		return 'footer-absolute'
+  	end
+
+  end
+
 end
