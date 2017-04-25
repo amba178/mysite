2e49e1f32bacb413063ae555801ecd978c589058
Salem Amba <salemamba@gmail.com>
2017-04-24 21:43:40 -0700

    updates video_uploader

--- app/controllers/video_uploads_controller.rb
+++ app/controllers/video_uploads_controller.rb
@@ -1,4 +1,5 @@
 class VideoUploadsController < ApplicationController
+  
   def new
   	@video_upload = VideoUpload.new
   end
@@ -8,16 +9,16 @@ class VideoUploadsController < ApplicationController
   	@video_upload = VideoUpload.new(title: params[:video_upload][:title],
                                   description: params[:video_upload][:description],
                                   file: params[:video_upload][:file].try(:tempfile).try(:to_path))
-  	# if @video_upload.save
-  		# uploaded_video = @video_upload.upload!(current_user)
-    #   uploaded_video = VideoJob.perform_async(@video_upload.id, current_user)
-      # byebug
-  		# if uploaded_video.failed? 
-  		# 	flash[:danger] = 'There was an error while uploading your video...'
-  		# else
-  		#   Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}"})
-  		#   flash[:notice] = 'Your video has been uploaded!'
-  		# end
+  	if @video_upload.save
+  		uploaded_video = @video_upload.upload!(current_user)
+      uploaded_video = VideoJob.perform_async(@video_upload.id, current_user)
+    
+  		if uploaded_video.failed? 
+  			flash[:danger] = 'There was an error while uploading your video...'
+  		else
+  		  Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}"})
+  		  flash[:notice] = 'Your video has been uploaded!'
+  		end
       redirect_to videos_url, notice: 'Your video is being uploading!'
 
   	else
