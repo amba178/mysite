8bdc9514cdc2f19b7d78c53929eaebfda4fc6995
Salem Amba <salemamba@gmail.com>
2017-04-25 11:42:37 -0700

    fixes the conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411035814) do
+ActiveRecord::Schema.define(version: 20170425040858) do
 
   create_table "charge_transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "charge_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170411035814) do
     t.string   "ip_address"
     t.string   "name"
     t.string   "card_type"
+    t.date     "card_expires_on"
     t.string   "billing_address"
     t.string   "zip_code"
     t.string   "city"
@@ -37,7 +38,6 @@ ActiveRecord::Schema.define(version: 20170411035814) do
     t.datetime "updated_at",      null: false
     t.integer  "user_id"
     t.integer  "amount"
-    t.string   "card_expires_on"
   end
 
   create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -69,12 +69,12 @@ ActiveRecord::Schema.define(version: 20170411035814) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -83,11 +83,11 @@ ActiveRecord::Schema.define(version: 20170411035814) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
+    t.integer  "failed_attempts",        default: 0,     null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "last_name"
     t.string   "first_name"
     t.string   "provider"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170411035814) do
     t.string   "image"
     t.string   "nickname"
     t.string   "auth_token"
+    t.boolean  "admin",                  default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
